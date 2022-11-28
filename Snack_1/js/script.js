6bff074403d3f881c37adece0fc05ff5426c8f5e
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-28 16:58:55 +0100

    snack n. 1

---
+++ Snack_1/js/script.js
@@ -0,0 +1,9 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+let userNumber = parseInt( prompt ('Inserisci un numero da 1 a 10') );
+
+while ( (userNumber < 1) || (userNumber > 10 )) {
+    userNumber = parseInt ( prompt('Devi inserire un numero dentro i parametri forniti!') );
+}
+
+console.log(userNumber);
